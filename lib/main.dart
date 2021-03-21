595a068bf54e541ae30c35ca9ae651e4466f0b8e
Amaan <azafar2000@gmail.com>
2021-03-22 04:45:00 +0530

    camera to preview and take image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:food_insta/components/custom_post_card.dart';
 import 'package:food_insta/models/user.dart';
@@ -7,7 +8,16 @@ import 'package:food_insta/screens/registration/registeration_page.dart';
 import 'package:food_insta/screens/registration/user_type.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
   runApp(MyApp());
 }
 
