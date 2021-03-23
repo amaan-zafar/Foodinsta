e8fcb49d07e8bac816dccec7ddaa5be31e29caa5
Amaan <azafar2000@gmail.com>
2021-03-23 09:07:52 +0530

    created custom scaffold to remove redundant code

--- lib/screens/registration/registeration_page.dart
+++ lib/screens/registration/registeration_page.dart
@@ -1,169 +1,118 @@
 import 'package:flutter/material.dart';
 import 'package:food_insta/components/custom_button.dart';
 import 'package:food_insta/components/custom_dropdown.dart';
+import 'package:food_insta/components/custom_scaffold.dart';
 import 'package:food_insta/components/custom_textfield.dart';
+import 'package:food_insta/theme.dart' as AppTheme;
 import 'package:food_insta/constants.dart' as Constants;
-import 'package:food_insta/models/user.dart';
-import 'package:food_insta/utils.dart';
 
-class RegisterationPage extends StatefulWidget {
-  final int userType;
-  const RegisterationPage({Key key, @required this.userType}) : super(key: key);
+class RegistrationForm extends StatefulWidget {
   @override
-  _RegisterUserState createState() => _RegisterUserState(userType);
+  _RegistrationFormState createState() => _RegistrationFormState();
 }
 
-class _RegisterUserState extends State<RegisterationPage> {
-  final int userType;
-  _RegisterUserState(this.userType);
+class _RegistrationFormState extends State<RegistrationForm> {
+  int userType = 0;
 
   bool isChecked = false;
+
   List<String> _states = Constants.STATE_CITY_JSON.keys.toList();
+
   String _selectedState = 'Assam';
+
   List<String> _cities = [];
+
   final _formKey = GlobalKey<FormState>();
-
-  @override
-  void initState() {
-    _selectedState = _states[0];
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              buildForm(userType),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 
   String _validate(String value, bool isRequired) {
     if (value.isEmpty && isRequired) return Constants.ERR_EMPTY_FIELD;
     return null;
   }
 
-  Widget buildForm(int userType) {
-    return Expanded(
-      child: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              SizedBox(height: 24),
-              Text(
-                'Fill ${Utils.getMemberType(userType)} details',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              CustomTextField(
-                keyboardType: TextInputType.name,
-                hintText: Constants.NAME_TEXT,
-                validator: (value) {
-                  return _validate(value, true);
-                },
-              ),
-              SizedBox(height: 24),
-              CustomTextField(
-                keyboardType: TextInputType.emailAddress,
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      children: [
+        SizedBox(height: 16),
+        Text(
+          'Final step, fill out your details',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        SizedBox(height: 16),
+        CustomTextField(
+          keyboardType: TextInputType.name,
+          hintText: Constants.NAME_TEXT,
+          validator: (value) {
+            return _validate(value, true);
+          },
+        ),
+        SizedBox(height: 16),
+        CustomTextField(
+          keyboardType: TextInputType.emailAddress,
+          hintText: '${Constants.EMAIL_TEXT} (${Constants.OPTIONAL_TEXT})',
+          validator: (value) {
+            return _validate(value, false);
+          },
+        ),
+        SizedBox(height: 16),
+        CustomDropDown(
+          list: _states,
+          value: _selectedState,
+          onChanged: null,
+        ),
+        SizedBox(height: 16),
+        CustomDropDown(
+          list: _states,
+          value: _selectedState,
+          onChanged: null,
+        ),
+        SizedBox(height: 16),
+        CustomTextField(
+          keyboardType: TextInputType.multiline,
+          hintText: Constants.STREET_ADDRESS,
+          validator: (value) {
+            return _validate(value, true);
+          },
+        ),
+        SizedBox(height: 16),
+        userType == 2
+            ? CustomTextField(
+                keyboardType: TextInputType.text,
                 hintText:
-                    '${Constants.EMAIL_TEXT} (${Constants.OPTIONAL_TEXT})',
+                    '${Constants.WEBSITE_TEXT} (${Constants.OPTIONAL_TEXT})',
                 validator: (value) {
                   return _validate(value, false);
                 },
-              ),
-              SizedBox(height: 24),
-              CustomDropDown(
-                list: _states,
-                value: _selectedState,
-                onChanged: null,
-              ),
-              SizedBox(height: 24),
-              CustomDropDown(
-                list: _states,
-                value: _selectedState,
-                onChanged: null,
-              ),
-              SizedBox(height: 24),
-              userType == 1
-                  ? CustomTextField(
-                      keyboardType: TextInputType.emailAddress,
-                      hintText: Constants.REGISTERATION_NUMBER_TEXT,
-                      validator: (value) {
-                        return _validate(value, true);
-                      },
-                    )
-                  : Row(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: CustomTextField(
-                            keyboardType: TextInputType.number,
-                            hintText: Constants.STD_CODE,
-                            validator: (value) {
-                              return _validate(value, false);
-                            },
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Expanded(
-                          flex: 3,
-                          child: CustomTextField(
-                            keyboardType: TextInputType.number,
-                            hintText:
-                                '${Constants.LANDLINE_TEXT} (${Constants.OPTIONAL_TEXT})',
-                            validator: (value) {
-                              return _validate(value, false);
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-              SizedBox(height: 24),
-              CustomTextField(
-                keyboardType: TextInputType.multiline,
-                hintText: Constants.STREET_ADDRESS,
+              )
+            : Container(),
+        SizedBox(height: 16),
+        userType == 1
+            ? CustomTextField(
+                keyboardType: TextInputType.emailAddress,
+                hintText: Constants.REGISTERATION_NUMBER_TEXT,
                 validator: (value) {
                   return _validate(value, true);
                 },
-              ),
-              SizedBox(height: 24),
-              userType == 2
-                  ? CustomTextField(
-                      keyboardType: TextInputType.text,
-                      hintText:
-                          '${Constants.WEBSITE_TEXT} (${Constants.OPTIONAL_TEXT})',
-                      validator: (value) {
-                        return _validate(value, false);
-                      },
-                    )
-                  : Container(),
-              CheckboxListTile(
-                title: Text(Constants.ACEEPT_TNC),
-                value: isChecked,
-                onChanged: (newValue) {
-                  setState(() {
-                    isChecked = newValue;
-                  });
-                },
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-            ],
-          ),
+              )
+            : Container(),
+        CheckboxListTile(
+          title: Text(Constants.ACEEPT_TNC),
+          value: isChecked,
+          onChanged: (newValue) {
+            // setState(() {
+            //   isChecked = newValue;
+            // });
+          },
+          controlAffinity: ListTileControlAffinity.leading,
         ),
-      ),
+        SizedBox(height: 16),
+        CustomButton(
+          onPressed: () {},
+          textOnButton: Constants.REGISTER_TEXT,
+          color: AppTheme.customButtonColor,
+        ),
+        SizedBox(height: 16),
+      ],
     );
   }
 }
