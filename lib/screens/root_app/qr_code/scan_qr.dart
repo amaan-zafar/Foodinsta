633a41f5479402e13874cedb396d20263de20c0d
Amaan <azafar2000@gmail.com>
2021-03-29 20:25:58 +0530

    updated scan qr code page

--- lib/screens/root_app/qr_code/scan_qr.dart
+++ lib/screens/root_app/qr_code/scan_qr.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:food_insta/components/custom_app_bar.dart';
 import 'package:food_insta/components/custom_background.dart';
 import 'package:food_insta/components/custom_card.dart';
 import 'package:food_insta/components/custom_icon_button.dart';
@@ -31,40 +32,13 @@ class _ScanQRState extends State<ScanQR> {
               child: Column(
             children: [
               // AppBar
-              SizedBox(
-                height: 74,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        Constants.APP_LABEL,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline1
-                            .copyWith(color: Colors.white),
-                      ),
-                      Spacer(),
-                      SizedBox(width: 10),
-                      CustomIconButton(
-                        onPressed: () {
-                          setState(() {
-                            pageIndex = 0;
-                          });
-                        },
-                        icon: Icon(
-                          Icons.arrow_back,
-                          color: Styles.iconColor,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+              CustomAppBar(
+                centerTitle: true,
               ),
 
               // Body
               Padding(
-                padding: EdgeInsets.fromLTRB(24, height * 0.15, 24, 8),
+                padding: EdgeInsets.fromLTRB(24, height * 0.09, 24, 8),
                 child: CustomAppCard(
                   width: double.infinity,
                   children: [
@@ -74,18 +48,16 @@ class _ScanQRState extends State<ScanQR> {
                       child: Text(
                         'Scan QR code from your food provider to complete the deal!',
                         textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18),
                       ),
                     ),
                     Icon(
                       MdiIcons.qrcode,
                       color: Styles.iconColor,
-                      size: 54,
+                      size: 60,
                     ),
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Center(
-                        child: CustomTextButton(
+                    SizedBox(height: 20),
+                    CustomTextButton(
                       textOnButton: 'Scan QR Code',
                       color: Styles.buttonColor2,
                       onPressed: () async {
@@ -100,7 +72,8 @@ class _ScanQRState extends State<ScanQR> {
                                 });
                         });
                       },
-                    ))
+                    ),
+                    SizedBox(height: 20),
                   ],
                 ),
               )
